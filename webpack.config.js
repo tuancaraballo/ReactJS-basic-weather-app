376ad1792d2793ba6dd3673b5db978ae5938cdec
Antuan Caraballo <tranc2606@gmail.com>
2017-07-06 06:38:35 -0700

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+	// where it should start processing your code
+	entry: './app/app.jsx',
+	// specify the output
+	output: {
+		path: __dirname, //-> specify the directory root
+		filename: './public/bundle.js'
+	},
+	// --> list of files we want to be able to process
+	resolve: {
+		extensions: ['*', '.js', '.jsx'],
+	//--> this let us import components by their name without having
+	  //  to specify their paths. ex:  require('Greeter') vs require(./components/Greeter)
+		alias: {
+			// Greeter: __dirname + '/public/components/Greeter.jsx',
+		},
+		
+	},
+	// --> adding module to be able to interpret the jsx extension
+	module: {
+		loaders: [
+			{
+				loader: 'babel-loader',
+				query: {
+					// -> take all the files, parse them through react and interpret them as
+					// 2015 js. How does it know which file? specify the test, see next
+					presets: ['react', 'es2015', 'stage-0']
+				},
+				test: /\.jsx?$/, //--> use this loader and parse all the files that end in jsx
+				exclude: /(node_modules|bower_components)/
+			}
+		]
+	}
+};
