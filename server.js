7b0ef504fd1cc8bcd0b44d64fb6d6e93f4f6a8cf
Antuan Caraballo <tranc2606@gmail.com>
2017-07-07 19:32:09 -0700

    initializing my heroku host and fixing the http request from openWeatherMap

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 var express = require('express');
 
 var app = express();
+const PORT = process.env.PORT ||  3000;
+
+app.use(function(request, response, next) {  //--> openWeatherMap always operates on http and not https, so for this app, I'm going
+											// to redirect all https traffic to http
+	if(request.headers['x-forwarded-proto'] === 'http'){ //-> if traffic is over http
+		next();
+	}else{
+		response.redirect('http://' + request.hostname + request.url);
+	}
+});
 
 // folder to expose to web server
 app.use(express.static('public'));
 
 
-app.listen('3000', function() {
-    console.log('Server listening to port 3000 ...');
+app.listen(PORT, function() {
+    console.log('Server listening to port '+ PORT);
 });
