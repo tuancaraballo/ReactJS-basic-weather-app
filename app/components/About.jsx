7e638c96f6910915a5e2b7d83f21edd04b120976
Antuan Caraballo <tranc2606@gmail.com>
2017-07-11 08:15:05 -0700

    styled about page and added resources I used

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,20 @@ var React = require('react');
 
 var About = (props) => {
 	return (
-		<h3> About Component </h3>
+		<div>
+			<h1 className="text-center">About Component </h1>
+			<p> This is a basic weather application I have built to learn ReactJS</p>
+			<p> Check out my  <a href="https://github.com/tuancaraballo/ReactJS-basic-weather-app">github repo </a> if you want to see the code </p>
+			<p> Some resources and apis I used include </p>
+			<ol>
+				<li>
+					<a href="https://facebook.github.io/react/"> ReactJS framework </a>
+				</li>
+				<li>
+					<a href="https://openweathermap.org/"> Open Weather Map </a>
+				</li>
+			</ol>
+		</div>
 	);
 }
 
